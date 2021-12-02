e18b2217da9e7a43f744324bc3d98ffc2adda347
lilibyte <lily@lilibyte.net>
2021-12-02 00:20:07 -0600

    2021: match case is fancy

--- 2021/02/washed.py
+++ 2021/02/washed.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3.10
 
-f = [l.strip() for l in __import__('fileinput').input(files=('input'))]
+f = [l.strip().split() for l in __import__('fileinput').input(files=('input'))]
 
 a = h = h2 = dp = dp2 = 0
 
-for l in f:
-    d, i = l.split()[0], int(l.split()[1])
+for d in f:
     match d:
-        case "forward":
-            h, h2 = (x + i for x in (h, h2))
-            dp2 += i * a
-        case "down":
-            dp, a = (x + i for x in (dp, a))
-        case "up":
-            dp, a = (x - i for x in (dp, a))
+        case ["forward", v]:
+            h, h2 = (x + int(v) for x in (h, h2))
+            dp2 += int(v) * a
+        case ["down", v]:
+            dp, a = (x + int(v) for x in (dp, a))
+        case ["up", v]:
+            dp, a = (x - int(v) for x in (dp, a))
 
 print(h * dp)
 print(h2 * dp2)
