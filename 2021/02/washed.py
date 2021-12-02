ba84338cf2973edb8d7fa54a511d0a6718c88da8
lilibyte <lily@lilibyte.net>
2021-12-02 04:35:40 -0600

    2021: remove redundant variable

--- 2021/02/washed.py
+++ 2021/02/washed.py
@@ -2,12 +2,12 @@
 
 f = [l.strip().split() for l in __import__('fileinput').input(files=('input'))]
 
-a = h = h2 = dp = dp2 = 0
+a = h = dp = dp2 = 0
 
 for d in f:
     match d:
         case ["forward", v]:
-            h, h2 = (x + int(v) for x in (h, h2))
+            h += int(v)
             dp2 += int(v) * a
         case ["down", v]:
             dp, a = (x + int(v) for x in (dp, a))
@@ -15,4 +15,4 @@ for d in f:
             dp, a = (x - int(v) for x in (dp, a))
 
 print(h * dp)
-print(h2 * dp2)
+print(h * dp2)
