a887046846768abe84ed5d963832ae1c03ccb89a
lilibyte <lily@lilibyte.net>
2021-12-07 01:31:07 -0600

    2021: make day 7 more efficient

--- 2021/07/07.py
+++ 2021/07/07.py
@@ -1,18 +1,19 @@
 f = sorted(int(i) for i in open("input").read().strip().split(","))
 
 m = f[len(f) // 2]
-l, p1, p2 = 0, 0, [0 for x in range(min(f), max(f) + 1)]
+l, p1, p2 = 0, 0, [0 for x in range(f[-1] + 1)]
 
 for cr in f:
     p1 += abs(m - cr)
 
-for i in range(min(f), max(f) + 1):
+for i in range(f[-1] + 1):
     for cr in f:
-        p2[i] += sum(range(1, abs(cr - i) + 1))
+        # thank you No.84646327-senpai
+        p2[i] += (abs(cr - i) + 1) * abs(cr - i) // 2
+        # p2[i] += sum(range(1, abs(cr - i) + 1))
     if l and l < p2[i]:
         break
     l = p2[i]
-
 
 print(p1)
 print(min(i for i in p2 if i > 0))
