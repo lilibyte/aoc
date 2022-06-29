46902ab58693be1359dd623033b9765ddcb35f80
lilibyte <lily@lilibyte.net>
2022-06-29 14:47:55 -0500

    lib: add python library

---
+++ lib/aoc.py
@@ -0,0 +1,23 @@
+from fileinput import input
+import re
+
+def atoi(s: str, base: int = None):
+	if isinstance(s, int):
+		return s
+	elif base:
+		return int(s, base)
+	elif s.isdigit():
+		return int(s)
+	else:
+		return s
+
+def scan(re_str: str):
+	r = re.compile(re_str)
+	for line in input():
+		for m in re.finditer(r, line):
+			if len(m.groups()) == 1:
+				yield atoi(m.group(1))
+			elif len(m.groups()) > 1:
+				yield tuple(atoi(t) for t in m.groups())
+			else:
+				yield line.strip()
